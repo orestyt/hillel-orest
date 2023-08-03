5759ebff157bf9d5251ad300b0f4a416441e14e7
Orest <orestyt@gmail.com>
2023-08-03 03:21:24 +0300

    Homework7 version one

---
+++ homework7/src/ArrayDataException.java
@@ -0,0 +1,8 @@
+public class ArrayDataException extends NumberFormatException{
+    public ArrayDataException() {
+    }
+
+    public ArrayDataException(String s) {
+        super(s);
+    }
+}
---
+++ homework7/src/ArraySizeException.java
@@ -0,0 +1,8 @@
+public class ArraySizeException extends NegativeArraySizeException {
+    public ArraySizeException() {
+    }
+
+    public ArraySizeException(String s) {
+        super(s);
+    }
+}
