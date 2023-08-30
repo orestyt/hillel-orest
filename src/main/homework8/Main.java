6af78d224bbec4e767fa68fdf3d38f6d6205a080
Orest <65716159+orestyt@users.noreply.github.com>
2023-08-30 13:18:18 +0300

    Homework8 (#7)
    * Homework8 some Test are still in progress
    
    * Homework8 some Test are still in progress
    
    * Homework8 some Test are still in progress

---
+++ src/main/homework8/Main.java
@@ -0,0 +1,58 @@
+package main.homework8;
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        List<String> stringList = new ArrayList<>();
+        stringList.add("apple");
+        stringList.add("pineaple");
+        stringList.add("apple");
+        stringList.add("bird");
+        stringList.add("bird");
+        stringList.add("pineaple");
+        stringList.add("bird");
+        System.out.println(findOccurance(stringList));
+
+    }
+
+    public static int countOccurance(List<String> stringList, String str) {
+        int count = 0;
+        for (String strs :
+                stringList) {
+            if (str.equals(strs)) count++;
+        }
+        return count;
+    }
+
+    public static <T> List<T> toList(T array[]) {
+        List<T> list = Arrays.asList(array);
+        return list;
+    }
+
+    public static List<Integer> findUnique(List<Integer> unsortedList) {
+        List<Integer> sortedList = new ArrayList<>(new LinkedHashSet<>(unsortedList));
+        return sortedList;
+    }
+
+    public static Map<String, Integer> calcOccurance(List<String> stringList) {
+        Map<String, Integer> occuranceMap = new HashMap();
+        for (String word : stringList) {
+            if (!occuranceMap.containsKey(word)) {
+                occuranceMap.put(word, 1);
+            } else {
+                occuranceMap.put(word, occuranceMap.get(word) + 1);
+            }
+        }
+        return occuranceMap;
+    }
+
+    public static StringBuilder findOccurance(List<String> stringList) {
+        StringBuilder occuranceString = new StringBuilder("[\n");
+        for (String key : calcOccurance(stringList).keySet()) {
+            occuranceString.append("{name: \"" + key + "\",occurrence: " + calcOccurance(stringList).get(key) + "},\n");
+        }
+        occuranceString.delete(occuranceString.length() - 2, occuranceString.length()).append("\n]");
+        return occuranceString;
+    }
+}
---
+++ src/test/homework8/MainTest.java
@@ -0,0 +1,79 @@
+package test.homework8;
+
+import main.homework8.Main;
+import org.junit.jupiter.api.Test;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest extends Main {
+
+    @Test
+    void shouldCountOccurance() {
+        List<String> stringList = new ArrayList<>();
+        stringList.add("apple");
+        stringList.add("pineaple");
+        stringList.add("apple");
+        stringList.add("bird");
+        stringList.add("bird");
+        stringList.add("pineaple");
+        stringList.add("bird");
+        assertEquals(3, countOccurance(stringList, "bird"));
+    }
+
+    @Test
+    void shouldToList() {
+    //Не розумію як розробити тест на цей метод
+    }
+
+    @Test
+    void shouldFindUnique() {
+        List<Integer> integerList = new ArrayList<>();
+        integerList.add(1);
+        integerList.add(3);
+        integerList.add(4);
+        integerList.add(2);
+        integerList.add(1);
+        integerList.add(2);
+        integerList.add(2);
+        integerList.add(1);
+        List<Integer> expectedList = new ArrayList<>();
+        expectedList.add(1);
+        expectedList.add(3);
+        expectedList.add(4);
+        expectedList.add(2);
+        assertArrayEquals(expectedList.toArray(), findUnique(integerList).toArray());
+
+    }
+
+    @Test
+    void shouldCalcOccurance() {
+        List<String> stringList = new ArrayList<>();
+        stringList.add("apple");
+        stringList.add("pineaple");
+        stringList.add("apple");
+        stringList.add("bird");
+        stringList.add("bird");
+        stringList.add("pineaple");
+        stringList.add("bird");
+        Map<String, Integer> expectedMap = new HashMap<>();
+        expectedMap.put("apple", 2);
+        expectedMap.put("pineaple", 2);
+        expectedMap.put("bird", 3);
+        assertEquals(expectedMap, calcOccurance(stringList));
+    }
+
+    @Test
+    void shouldFindOccurance() {
+        List<String> stringList = new ArrayList<>();
+        stringList.add("apple");
+        stringList.add("apple");
+        stringList.add("bird");
+        StringBuilder expectedString = new StringBuilder("[\n{name: \"apple\",occurrence: 2},\n{name: \"bird\",occurrence: 1}\n]");
+        assertEquals(expectedString.toString(), findOccurance(stringList).toString());
+    }
+}
