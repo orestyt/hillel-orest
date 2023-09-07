5228faf9c1cef59602c6505bf70f113512f5a3de
orestyt <orestyt@gmail.com>
2023-09-07 12:53:18 +0300

    Fixed

--- src/main/java/homework16/Product.java
+++ src/main/java/homework16/Product.java
@@ -11,7 +11,7 @@ public class Product {
     boolean canApplyDiscount;
     LocalDate date;
 
-    public Product(ProductType category, float price, boolean canApplyDiscount, LocalDate date) {
+    public Product(ProductType category, double price, boolean canApplyDiscount, LocalDate date) {
         this.category = category;
         this.price = price;
         this.canApplyDiscount = canApplyDiscount;
@@ -34,20 +34,17 @@ public class Product {
         return date;
     }
 
-    public void setCategory(ProductType category) {
-        this.category = category;
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Product product = (Product) o;
+        return Double.compare(price, product.price) == 0 && canApplyDiscount == product.canApplyDiscount && category == product.category && Objects.equals(date, product.date);
     }
 
-    public void setPrice(double price) {
-        this.price = price;
-    }
-
-    public void setCanApplyDiscount(boolean canApplyDiscount) {
-        this.canApplyDiscount = canApplyDiscount;
-    }
-
-    public void setDate(LocalDate date) {
-        this.date = date;
+    @Override
+    public int hashCode() {
+        return Objects.hash(category, price, canApplyDiscount, date);
     }
 
     @Override
@@ -61,48 +58,45 @@ public class Product {
     }
 
     public static List<Product> getHighPriceBook(List<Product> productList) {
-        var stream = productList.stream()
+        return productList.stream()
                 .filter(product -> product.getCategory() == ProductType.BOOK)
-                .filter(product -> product.getPrice() >= 250);
-        return stream.toList();
+                .filter(product -> product.getPrice() >= 250)
+                .toList();
     }
 
 
     public static List<Product> getDiscountBook(List<Product> productList) {
-        var stream = productList.stream()
+        return productList.stream()
                 .filter(product -> product.getCategory() == ProductType.BOOK)
                 .filter(product -> product.isCanApplyDiscount())
-                .map(product -> {
-                    product.setPrice(product.getPrice() - product.getPrice() / 10);
-                    return product;
-                });
-        return stream.toList();
+                .map(product -> new Product(product.getCategory(), product.getPrice() * 0.9, product.isCanApplyDiscount(), product.getDate()))
+                .toList();
     }
 
 
     public static List<Product> getLastThree(List<Product> productList) {
-        var stream = productList.stream()
-                .skip(productList.size() - 3);
-        return stream.toList();
+        return productList.stream()
+                .sorted(Comparator.comparing(Product::getDate, Comparator.nullsLast(Comparator.reverseOrder())))
+                .limit(3)
+                .toList();
     }
 
 
     public static Product getCheapestBook(List<Product> productList) throws Exception {
-        var stream = productList.stream()
+        return productList.stream()
                 .filter(product -> product.getCategory() == ProductType.BOOK)
                 .min(Comparator.comparingDouble(Product::getPrice))
                 .orElseThrow(Exception::new);
-        return stream;
     }
 
 
     public static int getSumCheapBook(List<Product> productList) {
-        var stream = (Integer) productList.stream()
+        return productList.stream()
                 .filter(product -> product.getCategory() == ProductType.BOOK)
                 .filter(product -> product.isWithinRange(product.getDate()))
                 .filter(product -> product.getPrice() <= 75)
-                .mapToInt(product -> (int) product.getPrice()).sum();
-        return stream;
+                .mapToInt(product -> (int) product.getPrice())
+                .sum();
     }
 
 
@@ -111,8 +105,9 @@ public class Product {
     }
 
 
-    public static Map<ProductType, List<Product>> groupProducts(List<Product> productList) {
-        var streamBook = productList.stream()
+
+    public static Map<ProductType, List<Product>> groupProducts(List<Product> productList) {        //Не дуже розумію
+        var streamBook = productList.stream()                                                       // як зробити краще
                 .filter(product -> product.getCategory() == ProductType.BOOK);
         var streamMilk = productList.stream()
                 .filter(product -> product.getCategory() == ProductType.MILK);
@@ -125,12 +120,12 @@ public class Product {
     public static void main(String[] args) {
         List<Product> products = new ArrayList<Product>();
         products.add(new Product(ProductType.BOOK, 75, true, LocalDate.now()));
-        products.add(new Product(ProductType.MILK, 50, false, LocalDate.now()));
-        products.add(new Product(ProductType.BOOK, 300, false, LocalDate.now()));
-        products.add(new Product(ProductType.BOOK, 400, true, LocalDate.now()));
-        products.add(new Product(ProductType.BOOK, 250, true, LocalDate.now()));
+        products.add(new Product(ProductType.MILK, 50, false, LocalDate.of(2019, Month.JULY, 12)));
+        products.add(new Product(ProductType.BOOK, 300, false, LocalDate.of(2020, Month.JULY, 12)));
+        products.add(new Product(ProductType.BOOK, 400, true, LocalDate.of(2021, Month.JULY, 12)));
+        products.add(new Product(ProductType.BOOK, 250, true, LocalDate.of(2022, Month.JULY, 12)));
         products.add(new Product(ProductType.BOOK, 60, true, LocalDate.of(1999, Month.APRIL, 2)));
         products.add(new Product(ProductType.BOOK, 25, true, LocalDate.of(2023, Month.JULY, 12)));
-        System.out.println(groupProducts(products));
+        System.out.println(getLastThree(products));
     }
 }
--- src/test/java/homework16/ProductTest.java
+++ src/test/java/homework16/ProductTest.java
@@ -5,7 +5,9 @@ import org.junit.jupiter.api.Test;
 import java.time.LocalDate;
 import java.time.Month;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import static homework16.Product.getHighPriceBook;
 import static org.junit.jupiter.api.Assertions.*;
@@ -22,7 +24,7 @@ class ProductTest {
         List<Product> expected = new ArrayList<>();
         expected.add(new Product(ProductType.BOOK, 300, false, LocalDate.now()));
         expected.add(new Product(ProductType.BOOK, 400, true, LocalDate.now()));
-        assertEquals(getHighPriceBook(products).toString(), expected.toString());
+        assertEquals(getHighPriceBook(products), expected);
     }
 
     @Test
@@ -34,24 +36,24 @@ class ProductTest {
         List<Product> expected = new ArrayList<>();
         expected.add(new Product(ProductType.BOOK, 90, true, LocalDate.now()));
         expected.add(new Product(ProductType.BOOK, 360, true, LocalDate.now()));
-        assertEquals(Product.getDiscountBook(products).toString(), expected.toString());
+        assertEquals(Product.getDiscountBook(products), expected);
     }
 
     @Test
     void shouldGetLastThree() {
         List<Product> products = new ArrayList<Product>();
         products.add(new Product(ProductType.BOOK, 75, true, LocalDate.now()));
-        products.add(new Product(ProductType.MILK, 50, false, LocalDate.now()));
-        products.add(new Product(ProductType.BOOK, 300, false, LocalDate.now()));
-        products.add(new Product(ProductType.BOOK, 400, true, LocalDate.now()));
-        products.add(new Product(ProductType.BOOK, 250, true, LocalDate.now()));
+        products.add(new Product(ProductType.MILK, 50, false, LocalDate.of(2019, Month.JULY, 12)));
+        products.add(new Product(ProductType.BOOK, 300, false, LocalDate.of(2020, Month.JULY, 12)));
+        products.add(new Product(ProductType.BOOK, 400, true, LocalDate.of(2021, Month.JULY, 12)));
+        products.add(new Product(ProductType.BOOK, 250, true, LocalDate.of(2022, Month.JULY, 12)));
         products.add(new Product(ProductType.BOOK, 60, true, LocalDate.of(1999, Month.APRIL, 2)));
         products.add(new Product(ProductType.BOOK, 25, true, LocalDate.of(2023, Month.JULY, 12)));
         List<Product> expected = new ArrayList<>();
-        expected.add(new Product(ProductType.BOOK, 250, true, LocalDate.now()));
-        expected.add(new Product(ProductType.BOOK, 60, true, LocalDate.of(1999, Month.APRIL, 2)));
+        expected.add(new Product(ProductType.BOOK, 75, true, LocalDate.now()));
         expected.add(new Product(ProductType.BOOK, 25, true, LocalDate.of(2023, Month.JULY, 12)));
-        assertEquals(Product.getLastThree(products).toString(), expected.toString());
+        expected.add(new Product(ProductType.BOOK, 250, true, LocalDate.of(2022, Month.JULY, 12)));
+        assertEquals(Product.getLastThree(products), expected);
     }
 
     @Test
@@ -66,7 +68,7 @@ class ProductTest {
         products.add(new Product(ProductType.BOOK, 25, true, LocalDate.of(2023, Month.JULY, 12)));
         Product expected = new Product(ProductType.BOOK, 25, true, LocalDate.of(2023, Month.JULY, 12));
         try {
-            assertEquals(Product.getCheapestBook(products).toString(), expected.toString().toString());
+            assertEquals(Product.getCheapestBook(products), expected);
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
@@ -88,5 +90,20 @@ class ProductTest {
 
     @Test
     void shouldGroupProducts() {
+        List<Product> products = new ArrayList<Product>();
+        products.add(new Product(ProductType.MILK, 50, false, LocalDate.of(2019, Month.JULY, 12)));
+        products.add(new Product(ProductType.BOOK, 300, false, LocalDate.of(2020, Month.JULY, 12)));
+        products.add(new Product(ProductType.BOOK, 400, true, LocalDate.of(2021, Month.JULY, 12)));
+        products.add(new Product(ProductType.BOOK, 250, true, LocalDate.of(2022, Month.JULY, 12)));
+        Map<ProductType, List<Product>> expected = new HashMap<>();
+        List<Product> milkProducts = new ArrayList<>();
+        milkProducts.add(new Product(ProductType.MILK, 50, false, LocalDate.of(2019, Month.JULY, 12)));
+        expected.put(ProductType.MILK, milkProducts);
+        List<Product> bookProducts = new ArrayList<>();
+        bookProducts.add(new Product(ProductType.BOOK, 300, false, LocalDate.of(2020, Month.JULY, 12)));
+        bookProducts.add(new Product(ProductType.BOOK, 400, true, LocalDate.of(2021, Month.JULY, 12)));
+        bookProducts.add(new Product(ProductType.BOOK, 250, true, LocalDate.of(2022, Month.JULY, 12)));
+        expected.put(ProductType.BOOK, bookProducts);
+        assertEquals(expected, Product.groupProducts(products));
     }
 }
