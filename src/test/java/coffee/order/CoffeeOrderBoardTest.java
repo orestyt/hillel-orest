a6b6d5d8f5e9d687abcaa92c6a394da72ff1e413
orest <orestyt@gmail.com>
2023-11-02 15:09:31 +0200

    homework10

---
+++ src/test/java/coffee/order/CoffeeOrderBoardTest.java
@@ -0,0 +1,31 @@
+package coffee.order;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CoffeeOrderBoardTest {
+    private final CoffeeOrderBoard coffeeOrderBoard = new CoffeeOrderBoard();
+
+    @Test
+    void shouldAdd() {
+        coffeeOrderBoard.add("Vlad");
+        coffeeOrderBoard.add("Nika");
+        List<Order> shouldOrderList = coffeeOrderBoard.getOrderList();
+        Order order1 = shouldOrderList.get(0);
+        assertEquals("Vlad", order1.getName());
+    }
+
+    @Test
+    void shouldDeliver() {
+        coffeeOrderBoard.add("Vlad");
+        coffeeOrderBoard.add("Nika");
+        List<Order> shouldOrderList = coffeeOrderBoard.getOrderList();
+        coffeeOrderBoard.deliver();
+        Order order1 = shouldOrderList.get(0);
+        assertEquals("Nika", order1.getName());
+    }
+}
